bcf5ba4c0bdb8a6c9465b48afbbeef18a2942ed4
hsenmrad <husseinmrad.business@gmail.com>
2025-02-21 17:50:52 +0200

    initial commit

--- Nabiha.py
+++ Nabiha.py
@@ -1,2 +1,7 @@
 #Nabiha Salary
-Salary=input(Enter your Salary :)
+Salary=float(input("Enter your Salary :"))
+Month=input("En(ter the name of Month:")
+
+Saving=float(input("Enter Your Saving percentage:"))
+Rent=float(input("Enter Your Rent percentage:"))
+Electricity=float(input("Enter Your Rent percentage:"))
